b299ba6e59505e672cb5564831397b2213be648c
Jakub Olejnik <jacobole2000@gmail.com>
2025-04-09 10:20:01 +0200

    Logic to save and get token from app memory

--- App.tsx
+++ App.tsx
@@ -4,6 +4,24 @@ import AsyncStorage from '@react-native-async-storage/async-storage';
 import { useEffect, useState } from 'react';
 
 export default function App() {
+
+  const [value, setValue] = useState('');
+  const [token, setToken] = useState('');
+
+  useEffect(() => {
+    const getToken = async () => {
+      const token = await AsyncStorage.getItem('userToken') || '';
+      setToken(token);
+    }
+    getToken();
+  }, []);
+
+  const handleSaveToken = async () => {
+    await AsyncStorage.setItem('userToken', value);
+    const token = await AsyncStorage.getItem('userToken') || '';
+    setToken(token);
+  }
+
   return (
     <View style={styles.container}>
       <Text>Open up App.tsx to start working on your app!</Text>
