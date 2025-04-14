1486800768f3776a220d0863c9f75e1cb37d9d86
Jakub Olejnik <jacobole2000@gmail.com>
2025-04-14 14:39:19 +0200

    Replaced App.tsx with Auth component

--- App.tsx
+++ App.tsx
@@ -3,36 +3,12 @@ import { Button, StyleSheet, Text, TextInput, View } from 'react-native';
 import AsyncStorage from '@react-native-async-storage/async-storage';
 import { useEffect, useState } from 'react';
 import { CameraView, Camera } from "expo-camera";
+import Auth from './auth/Auth';
 
 export default function App() {
 
   return (
-    <View style={styles.container}>
-      <StatusBar style="auto" />
-      {token ? <Text>{token}</Text> : <Text>Brak tokena</Text>}
-      <>
-        <Text>Open up App.tsx to start working on your app!</Text>
-        <TextInput value={value} onChangeText={(text) => setValue(text)} placeholder='text'></TextInput>
-        <Button title='Zatwierdź' onPress={() => handleSaveToken()} />
-      </>
-
-      {hasPermission && !token ? (
-        <View style={{ flex: 1, width: '100%' }}>
-          <CameraView
-            onBarcodeScanned={scanned ? undefined : handleBarcodeScanned}
-            barcodeScannerSettings={{
-              barcodeTypes: ["qr", "pdf417"],
-            }}
-            style={{ flex: 1 }}
-          />
-          {scanned && (
-            <Button title={"Skanuj ponownie"} onPress={() => setScanned(false)} />
-          )}
-        </View>
-      ) : (
-        <Text>Brak dostępu do kamery</Text>
-      )}
-    </View>
+    <Auth />
   );
 }
 
