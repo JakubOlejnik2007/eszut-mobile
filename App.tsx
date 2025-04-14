c3e08af080530090b534ebdeacac9e17eaeac34e
Jakub Olejnik <jacobole2000@gmail.com>
2025-04-14 14:38:16 +0200

    Moved qr scanning logic from App.tsx to LoginScreen.tsx

--- App.tsx
+++ App.tsx
@@ -6,41 +6,6 @@ import { CameraView, Camera } from "expo-camera";
 
 export default function App() {
 
-  const [value, setValue] = useState('');
-  const [token, setToken] = useState('');
-
-  useEffect(() => {
-    const getToken = async () => {
-      const token = await AsyncStorage.getItem('userToken') || '';
-      setToken(token);
-    }
-    getToken();
-  }, []);
-
-  const [hasPermission, setHasPermission] = useState<boolean | null>(null);
-  const [scanned, setScanned] = useState(false);
-
-  useEffect(() => {
-    const getCameraPermissions = async () => {
-      const { status } = await Camera.requestCameraPermissionsAsync();
-      setHasPermission(status === "granted");
-    };
-
-    getCameraPermissions();
-  }, []);
-
-  const handleBarcodeScanned = async ({ type, data }: { type: any, data: any }) => {
-    setScanned(true);
-    alert(`Bar code with type ${type} and data ${data} has been scanned!`);
-    await handleSaveToken(data);
-  };
-
-  const handleSaveToken = async (new_token: string = '') => {
-    await AsyncStorage.setItem('userToken', new_token || value);
-    const token = await AsyncStorage.getItem('userToken') || '';
-    setToken(token);
-  }
-
   return (
     <View style={styles.container}>
       <StatusBar style="auto" />
--- views/LoginScreen.tsx
+++ views/LoginScreen.tsx
@@ -1,6 +1,36 @@
-import { View, Text } from "react-native"
+import { Camera, CameraView } from "expo-camera";
+import { useEffect, useState } from "react";
+import { View, Text, Button, StatusBar, TextInput } from "react-native"
+import { useAuth } from "../auth/Auth";
 
 const LoginScreen = () => {
+    const { addNewToken, token } = useAuth();
+
+    const [hasPermission, setHasPermission] = useState<boolean | null>(null);
+
+    useEffect(() => {
+        const getCameraPermissions = async () => {
+            const { status } = await Camera.requestCameraPermissionsAsync();
+            setHasPermission(status === "granted");
+        };
+
+        getCameraPermissions();
+    }, []);
+
+    const [scanned, setScanned] = useState(false);
+
+    const handleBarcodeScanned = async ({ type, data }: { type: any, data: any }) => {
+        setScanned(true);
+        alert(`Bar code with type ${type} and data ${data} has been scanned!`);
+        await handleSaveToken(data);
+    };
+
+    const handleSaveToken = async (new_token: string = '') => {
+        addNewToken(new_token)
+    }
+
+    console.log(token)
+
     return (
         <>
             <View>
