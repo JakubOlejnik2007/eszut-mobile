4c0d2bfa0090978611fa116472098dfb411398c5
Jakub Olejnik <jacobole2000@gmail.com>
2025-04-15 09:12:01 +0200

    Removed unused code from App.tsx

--- App.tsx
+++ App.tsx
@@ -1,22 +1,7 @@
-import { StatusBar } from 'expo-status-bar';
-import { Button, StyleSheet, Text, TextInput, View } from 'react-native';
-import AsyncStorage from '@react-native-async-storage/async-storage';
-import { useEffect, useState } from 'react';
-import { CameraView, Camera } from "expo-camera";
 import Auth from './auth/Auth';
 
 export default function App() {
-
   return (
     <Auth />
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
