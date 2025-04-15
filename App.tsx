508dec9502ac0e7da95d4a1ae9833bf8928d068d
Jakub Olejnik <jacobole2000@gmail.com>
2025-04-15 09:37:15 +0200

    Added safe area (handle iOS and Android status bars)

--- App.tsx
+++ App.tsx
@@ -1,7 +1,13 @@
+import { StatusBar, SafeAreaView } from 'react-native';
 import Auth from './auth/Auth';
 
 export default function App() {
   return (
-    <Auth />
+    <>
+      <StatusBar barStyle="dark-content" backgroundColor="#f1b3cc" />
+      <SafeAreaView style={{ flex: 1, backgroundColor: "#f1b3cc" }}>
+        <Auth />
+      </SafeAreaView>
+    </>
   );
 }
