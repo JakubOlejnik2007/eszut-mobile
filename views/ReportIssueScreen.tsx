bd24eb89f6229b3d26474c9b55cd19ae33a2a8ad
Jakub Olejnik <jacobole2000@gmail.com>
2025-04-29 09:42:49 +0200

    Added ReportIssueScreen component

---
+++ views/ReportIssueScreen.tsx
@@ -0,0 +1,13 @@
+import { View, Text } from "react-native"
+
+const ReportIssueScreen = () => {
+    return (
+        <>
+            <View>
+                <Text>Zgłoś usterkę</Text>
+            </View>
+        </>
+    )
+}
+
+export default ReportIssueScreen;
