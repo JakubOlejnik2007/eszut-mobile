bd08126487c331e7477eff6733232c201b03d3cb
Jakub Olejnik <jacobole2000@gmail.com>
2025-04-30 13:56:16 +0200

    Added necessary imports in ReportIssueScreen

--- views/ReportIssueScreen.tsx
+++ views/ReportIssueScreen.tsx
@@ -1,4 +1,12 @@
 import { View, Text } from "react-native"
+import { Picker } from "@react-native-picker/picker"
+import { useState } from "react"
+import { useQuery } from "@tanstack/react-query"
+
+import axios from 'axios';
+import { getCategories, getPlaces } from "../service/apiFetchFunctions";
+
+
 
 const ReportIssueScreen = () => {
     return (
