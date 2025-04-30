31c3d24df5b2ea8c38b9fd82c5e87930856bb818
Jakub Olejnik <jacobole2000@gmail.com>
2025-04-30 13:57:55 +0200

    Added useQuery logic to ReportIssueScreen

--- views/ReportIssueScreen.tsx
+++ views/ReportIssueScreen.tsx
@@ -9,6 +9,37 @@ import { getCategories, getPlaces } from "../service/apiFetchFunctions";
 
 
 const ReportIssueScreen = () => {
+    const [categoriesQuery, placesQuery] = [
+        useQuery({
+            queryKey: ["categories"],
+            queryFn: getCategories
+        }),
+        useQuery({
+            queryKey: ["places"],
+            queryFn: getPlaces
+        })
+    ]
+
+
+
+    const [selectedValue, setSelectedValue] = useState("")
+
+
+
+    if (categoriesQuery.isError || placesQuery.isError) {
+        console.log("Categories Error:", {
+            message: categoriesQuery.error?.message,
+            name: categoriesQuery.error?.name
+        });
+
+        console.log("Places Error:", {
+            message: placesQuery.error?.message,
+            name: placesQuery.error?.name
+        });
+    }
+
+
+
     return (
         <>
             <View>
